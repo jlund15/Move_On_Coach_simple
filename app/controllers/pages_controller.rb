d5cfcd3582d52de2d6e7d5e65acddbe7ca030bb4
Jared Lund <jared.lund@icloud.com>
2017-07-06 20:13:55 -0600

    created new rails file

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def my_story
+  end
+
+  def coaching
+  end
+
+  def get_started
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  resources :blogs
+  get 'pages/home'
+
+  get 'pages/my_story'
+
+  get 'pages/coaching'
+
+  get 'pages/get_started'
+
+  get 'pages/contact'
+
+  root 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
